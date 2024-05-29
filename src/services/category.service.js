4628fdf7596bea4cde6f63fb23790e3603a54995
Udit Aggarwal <udit.aggarwal@gmail.com>
2024-05-29 14:15:36 +0530

    modified:   src/services/category.service.js

--- src/services/category.service.js
+++ src/services/category.service.js
@@ -9,46 +9,46 @@ const uploadFolder = process.env.UPLOAD_FOLDER || '/var/www/html/media';
 
 
 const createCategory = async (req) => {
-  console.log( "Hello file", req.files['slide_show[]'])
-  if(req.files.banner){
+  console.log("Hello file", req.files['slide_show[]'])
+  if (req.files.banner) {
 
-  var banner = uploadSingleFile(req.files.banner)
+    var banner = uploadSingleFile(req.files.banner)
   }
-  if(req.files.icon){
+  if (req.files.icon) {
 
     var icon = uploadSingleFile(req.files.icon)
   }
-  if(req.files['slide_show[]']){
+  if (req.files['slide_show[]']) {
     var slide_show = uploadMultipleFile(req.files['slide_show[]'])
   }
-  if(slide_show){
+  if (slide_show) {
 
     var slideShowValues = slide_show.map(item => item.value);
   }
   // console.log(slideShowValues)
-const slug = generateSlug(req.body.name);
-   
-    const category = new Category({
-      name: req.body.name,
-      // code: req.body.code,
-      banner:banner&& banner,
-      icon: icon && icon,
-      slide_show:slideShowValues,
-      description:req.body.description,
-      parent_category:req.body.parent_category,
-      meta_title: req.body.meta_title,
-      meta_description: req.body.meta_description,
-      slug: slug
-      
-    })
-    const newCategory = category.save()
-    return newCategory;
-    
+  const slug = generateSlug(req.body.name);
+
+  const category = new Category({
+    name: req.body.name,
+    // code: req.body.code,
+    banner: banner && banner,
+    icon: icon && icon,
+    slide_show: slideShowValues,
+    description: req.body.description,
+    parent_category: req.body.parent_category,
+    meta_title: req.body.meta_title,
+    meta_description: req.body.meta_description,
+    slug: slug
+
+  })
+  const newCategory = category.save()
+  return newCategory;
+
 };
 
 
 const queryCategories = async (filter, options) => {
-//   const categories = await Category.paginate(filter, options);
+  //   const categories = await Category.paginate(filter, options);
   return Category.find();
 };
 
@@ -59,74 +59,73 @@ const getCategoryById = async (id) => {
 
 
 
-const updateCategoryById = async (req,categoryId, updateBody) => {
-   console.log( "Updated", updateBody)
-  console.log( "categryId",categoryId)
+const updateCategoryById = async (req, categoryId, updateBody) => {
+  console.log("Updated", updateBody)
+  console.log("categryId", categoryId)
   console.log(req.files)
- 
+
   const category = await getCategoryById(categoryId);
   if (!category) {
     throw new ApiError(httpStatus.NOT_FOUND, 'Category not found');
   }
 
 
-  if(req.files.banner&& Object.keys(req.files.banner).length !== 0 && typeof req.files.banner === 'object') {
+  if (req.files.banner && Object.keys(req.files.banner).length !== 0 && typeof req.files.banner === 'object') {
     console.log(req.files);
     const banner = uploadSingleFile(req.files.banner)
-    category.banner =  banner;
+    category.banner = banner;
   } else {
     updateBody.banner = category.banner
   }
 
-  if(req.files.icon&& Object.keys(req.files.icon).length !== 0 && typeof req.files.icon === 'object') {
+  if (req.files.icon && Object.keys(req.files.icon).length !== 0 && typeof req.files.icon === 'object') {
     console.log(req.files);
     const icon = uploadSingleFile(req.files.icon)
-    category.icon =  icon;
+    category.icon = icon;
   } else {
     updateBody.icon = category.icon
   }
 
- var length=0
+  var length = 0
 
 
-  if(!updateBody.slide_show){
- 
-    category.slide_show=[]
-  } 
-  
-// console.log( "hello length",req.files.slide_show.length)
-      
+  if (!updateBody.slide_show) {
+    category.slide_show = []
+  }
+
+  // console.log( "hello length",req.files.slide_show.length)
+
   if (req.files['slide_show[]']) {
- var slide_showImage = uploadMultipleFile(req.files['slide_show[]']);
-var slideShowValues = slide_showImage.map(item => item.value);
+    var slide_showImage = uploadMultipleFile(req.files['slide_show[]']);
+    var slideShowValues = slide_showImage.map(item => item.value);
     // Check if slide_show array exists in updateBody
     if (!updateBody.slide_show || !Array.isArray(updateBody.slide_show)) {
-        updateBody.slide_show = []; // Initialize slide_show as an array if it doesn't exist
+      updateBody.slide_show = []; // Initialize slide_show as an array if it doesn't exist
     }
 
     // Add the new slide_showImage to the slide_show array
-   
-    
-  
+
+
+
     // updateBody.slide_show.push(slideShowValues);
     //  updateBody.slide_show=slideShowValues
     updateBody.slide_show = [...updateBody.slide_show, ...slideShowValues];
-}
+  }
 
-// else{
-//   updateBody.slide_show = [...updateBody.slide_show]
-// }
+  // else{
+  //   updateBody.slide_show = [...updateBody.slide_show]
+  // }
 
 
-    
-    
-    Object.assign(category, updateBody);
-    
-    console.log( "ajaja", category)
-   
- 
- 
-  
+
+
+  Object.assign(category, updateBody);
+
+  console.log("--------------------", category)
+
+
+
+
   await category.save();
 
   return category;
@@ -139,45 +138,45 @@ const deleteCategoryById = async (categoryId) => {
   if (!category) {
     throw new ApiError(httpStatus.NOT_FOUND, 'Category not found');
   }
-  const imageName = category.banner; 
+  const imageName = category.banner;
   const iconName = category.icon;
   // Construct the path to the image file
 
-  const imagePath =imageName&& path.join(uploadFolder, imageName);
-  const iconPath =iconName&& path.join(uploadFolder, iconName);
+  const imagePath = imageName && path.join(uploadFolder, imageName);
+  const iconPath = iconName && path.join(uploadFolder, iconName);
 
 
   // Delete the image file from the file system
-  if(imagePath){
-
-  
-  fs.unlink(imagePath, (err) => {
-    if (err) {
-      console.error('Error deleting image:', err);
-      return res.status(500).json({ error: 'Failed to delete image' });
-    }
-  })
-}
-if(iconPath){
+  if (imagePath) {
 
 
-  fs.unlink(iconPath, (err) => {
-    if (err) {
-      console.error('Error deleting icon:', err);
-      return res.status(500).json({ error: 'Failed to delete icon' });
-    }
-  })
- }
+    fs.unlink(imagePath, (err) => {
+      if (err) {
+        console.error('Error deleting image:', err);
+        return res.status(500).json({ error: 'Failed to delete image' });
+      }
+    })
+  }
+  if (iconPath) {
+
+
+    fs.unlink(iconPath, (err) => {
+      if (err) {
+        console.error('Error deleting icon:', err);
+        return res.status(500).json({ error: 'Failed to delete icon' });
+      }
+    })
+  }
   await category.remove();
   return category;
 };
 
 
 module.exports = {
-    createCategory,
-    queryCategories,
-    getCategoryById,
-    updateCategoryById,
-    deleteCategoryById,
-  
+  createCategory,
+  queryCategories,
+  getCategoryById,
+  updateCategoryById,
+  deleteCategoryById,
+
 };
