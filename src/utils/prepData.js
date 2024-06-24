f798a353079b8eef869312ea0696659482d8fb70
Udit Aggarwal <udit.aggarwal@gmail.com>
2024-06-24 21:19:26 +0530

    modified:   src/controllers/store/customer.controller.js 	new file:   src/utils/prepData.js
    	modified:   src/controllers/store/customer.controller.js
    	modified:   src/controllers/store/home.controller.js
    	new file:   src/utils/prepData.js

---
+++ src/utils/prepData.js
@@ -0,0 +1,41 @@
+const MEDIA_URL = process.env.MEDIA_URL;
+
+const prepareOrderItems = (items) => {
+
+    if (!Array.isArray(items)) {
+      return [];
+    }
+  
+    return items.map(item => {
+      const {
+        variant: { _id, ...variantRest },
+        productId,
+        name,
+        sku,
+        image,
+        quantity,
+        price,
+        discounted_price,
+        total
+      } = item;
+  
+      return {
+        variant: {
+          ...variantRest,
+          variant_id: _id,
+          image: variantRest.image || variantRest.image=="undefined" ? `${MEDIA_URL}${variantRest.image}` : ""
+        },
+        Product_id: productId,
+        name,
+        sku,
+        image: `${MEDIA_URL}${image}`,
+        quantity,
+        price,
+        discounted_price,
+        total
+      };
+    });
+  }
+
+  
+  module.exports = { prepareOrderItems };
